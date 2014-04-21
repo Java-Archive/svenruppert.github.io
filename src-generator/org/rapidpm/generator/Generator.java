ae2711969debe05b6d15c034ed2bd4066ebe1c96
Sven Ruppert <sven.ruppert@rapidpm.org>
2014-04-21 13:28:02 +0200

    added team area

--- src-generator/org/rapidpm/generator/Generator.java
+++ src-generator/org/rapidpm/generator/Generator.java
@@ -1,9 +1,11 @@
 package org.rapidpm.generator;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
